6fd9748f9f80ceb34decaa5bd1719e5a84cd7ffb
Ian Robinson <ianronrobinson@gmail.com>
2025-07-20 06:33:26 -0400

    fix: Put test endpoint before all consuming slug

--- core/api.py
+++ core/api.py
@@ -11,10 +11,12 @@ if settings.sentry_dsn:
     sentry_sdk.init(dsn=settings.sentry_dsn, send_default_pii=False)
 
 app = FastAPI()
-app.include_router(stats_router)
-app.include_router(shortlinks_router)
 
 
 @app.get("/sentry-debug")
 async def trigger_error() -> None:
     division_by_zero = 1 / 0  # pyright: ignore[reportUnusedVariable]  # noqa: F841
+
+
+app.include_router(stats_router)
+app.include_router(shortlinks_router)
