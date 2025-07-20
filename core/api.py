f07070989c141a1b6afb5beacda4b300ea2b3213
Ian Robinson <ianronrobinson@gmail.com>
2025-07-20 06:35:48 -0400

    fix: Fix horrible glaring issue

--- core/api.py
+++ core/api.py
@@ -12,11 +12,5 @@ if settings.sentry_dsn:
 
 app = FastAPI()
 
-
-@app.get("/sentry-debug")
-async def trigger_error() -> None:
-    division_by_zero = 1 / 0  # pyright: ignore[reportUnusedVariable]  # noqa: F841
-
-
 app.include_router(stats_router)
 app.include_router(shortlinks_router)
